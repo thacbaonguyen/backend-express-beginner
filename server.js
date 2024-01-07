cf5856610e3c4a4fe0471cf70c405d73a1b21b3a
thacbaonguyen <ar.thbaonguyen@gmail.com>
2024-01-08 01:37:20 +0700

    lesson3

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const port = process.env.PORT || 8080;
 app.set("views", path.join(__dirname, "./src/view"));
 app.set("view engine", "ejs");
 const hostName = process.env.HOST_NAME;
+// config static files
 
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! with Bao");
 });
 app.get("/abc", (req, res) => {
   res.render("sample.ejs");
 });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
