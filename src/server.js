26e317ab4371d0e0ec04aff2ba0aaa5f832a22ef
thacbaonguyen <ar.thbaonguyen@gmail.com>
2024-01-08 02:31:02 +0700

    lesson 4

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 require("dotenv").config();
 // console.log("check env: ", process.env.PORT);
 const port = process.env.PORT || 8080;
-app.set("views", path.join(__dirname, "./src/view"));
+app.set("views", path.join(__dirname, "view"));
 app.set("view engine", "ejs");
 const hostName = process.env.HOST_NAME;
 // config static files
-
-app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.send("Hello World! with Bao");
+  res.send("Hello World! with Baos");
 });
 app.get("/abc", (req, res) => {
   res.render("sample.ejs");
