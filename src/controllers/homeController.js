ec21e6c2cf16b5db2367660c99bd9f71f74b2907
thacbaonguyen <ar.thbaonguyen@gmail.com>
2024-01-08 21:48:53 +0700

    navbar design

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,10 @@
 const pool = require("../config/database");
 const getHomePage = (req, res) => {
-  pool.query("SELECT * FROM Users u", function (error, results, fields) {
-    const dataString = JSON.stringify(results);
-    res.send(dataString);
-  });
+  // pool.query("SELECT * FROM Users u", function (error, results, fields) {
+  //   const dataString = JSON.stringify(results);
+  //   res.send(dataString);
+  // });
+  res.render("home.ejs");
 };
 const getABC = (req, res) => {
   res.render("sample.ejs");
